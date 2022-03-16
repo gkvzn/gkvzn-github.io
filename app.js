a4d7563a16bdcc61078600154d77ab0a5bd8b84b
gkvzn <gokulsrinireddy@gmail.com>
2022-03-16 18:27:19 +0100

    menu

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const navSlide = ()=>{
     const burger = document.querySelector('.burger');
-    const nav =document.querySelector('nav ul');
-    const navlinks = document.querySelectorAll('nav ul li');
+    const nav = document.querySelector('.nav ul');
+    const navlinks = document.querySelectorAll('.nav ul li');
     //toggle links
-    burger.addEventListener('click',() =>{
+    burger.addEventListener('click', () =>{
         nav.classList.toggle('nav-action');
 
     });
     //ANIMATE LINKS
     navlinks.forEach((link, index)=> {
-        link.style.animation = 'navlinkfade 0.5s ease forwards ${index / 7}s';
+        link.style.animation = 'navlinkFade 0.5s ease forwards ${index / 7}s';
         console.log(index / 7);
 
     });
